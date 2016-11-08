83215c55ce8ed6b450d26f5844dcd439d8c7cd49
PlusA2M <plusa2m@gmail.com>
2016-11-08 18:33:33 +0800

    Initial Commit

---
+++ magnetic-indoor-positioning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  magnetic-indoor-positioning
+//
+//  Created by meiinlam on 8/11/2016.
+//  Copyright © 2016年 plusa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
